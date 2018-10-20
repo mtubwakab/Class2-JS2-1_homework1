791cddd97ac8945638a3b0930896a160c0020e5e
prabhamanju <43313631+prabhamanju@users.noreply.github.com>
2018-10-21 01:08:11 +0200

    updaed some code

--- manju_module2_JS/apps.js
+++ manju_module2_JS/apps.js
@@ -1,7 +1,14 @@
+// creating Objects for books
 let books = {
         title1: { title: 'chamber secrets', language: 'English', author: 'harry potter'},
         title2: { title: 'the golden bowl', language: 'english', author: 'henry james'},
         title3: { title: 'a passage to India', language: 'english', author: 'em forste'},
+        title4: { title: 'War and Peace', language: 'English', author: 'Leo Tolstoy'},
+        title5: { title: 'The Odyssey', language: 'english', author: 'Homer'},
+        title6: { title: 'Crime and Punishment ', language: 'english', author: 'Fyodor Dostoyevsky'},
+        title7: { title: 'alice in Wonderland', language: 'English', author: 'Lewis Carroll'},
+        title8: { title: 'Pride and Prejudice', language: 'english', author: 'Jane Austen'},
+        title9: { title: 'To the Lighthouse', language: 'english', author: 'Virginia Woolf'},
 // //      title4: { title: ' breathing_lessons_', language: 'English', author: 'anne tyler'}
 }
 
