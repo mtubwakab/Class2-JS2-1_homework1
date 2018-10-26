759367ba85623b35b732a6a5dd9eecf06ded3b60
alam <malafarzu@gmail.com>
2018-10-26 16:48:14 +0200

    presenting homework map_filter

---
+++ Farzu/MapAndFilter/Maartj's.js
@@ -0,0 +1,31 @@
+const monday = [
+    {name: 'Write a summary HTML/CSS',duration: 180},
+    {name: 'Some web development',duration: 120},
+    {name: 'Fix homework for class10',duration: 20},
+    {name: 'Talk to a lot of people',duration: 200},
+  ];
+  
+  const tuesday = [
+    {name: 'Keep writing summary',duration: 240},
+    {name: 'Some more web development',duration: 180},
+    {name: 'Staring out the window',duration: 10},
+    {name: 'Talk to a lot of people',duration: 200},
+    {name: 'Look at application assignments new students',duration: 40},
+  ];
+  
+  const tasks = monday.concat(tuesday);
+
+
+  const workInHours = tasks.map((minutes) => minutes.duration / 60 );
+  
+  const greaterThan2Hours = workInHours.filter((time) => time >=2 );
+
+  const payment = greaterThan2Hours.map((amount) => amount * 20 );
+   
+  const sum = payment.reduce((accumulate, value) => accumulate + value );
+
+  const totalInEuros = "€ " + sum.toFixed(3);
+  
+  
+  console.log(totalInEuros);
+  
